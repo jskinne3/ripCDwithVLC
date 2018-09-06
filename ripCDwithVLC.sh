0e1d91b9b6c3055bb97fea076a73c740b3b9db54
John Skinner <jno.skinner@gmail.com>
2018-09-06 17:32:04 -0500

    Add comments, move magic number into var

--- ripCDwithVLC.sh
+++ ripCDwithVLC.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-# Rip every track on a CD to a .ogg file
+# Rip every track on a CD to an .ogg file
 # Uses MacOS and VLC media player
+
+# Set the type of file to save
+bitrate=256
+format=vorb
 
 echo Enter artist name
 read artist
 echo Enter album name
 read album
 
+# Create a new dir to hold music in the user's current dir
 mkdir $artist
-
-#cd /Volumes/Audio\ CD/
 
 for f in /Volumes/Audio\ CD/*
 do
 
+  # Track numbers are extracted from the source file names
   number=$(printf "%02d" ${f//[!0-9]/})
   trackname=$album\_$number
   pathname=$artist/$trackname
 
-  echo ==== Ripping track $f as file $pathname
+  echo ==== Ripping $f as $pathname
   
-  /Applications/VLC.app/Contents/MacOS/VLC -I dummy "$f" ":sout=#transcode{acodec=vorb,ab=256}:std{dst=$pathname.ogg,access=file}" vlc://quit
+  # Open VLC, rip music, and close
+  /Applications/VLC.app/Contents/MacOS/VLC -I dummy "$f" ":sout=#transcode{acodec=$format,ab=$bitrate}:std{dst=$pathname.ogg,access=file}" vlc://quit
 
 done
